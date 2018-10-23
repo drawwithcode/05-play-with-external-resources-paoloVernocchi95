160fc4666a0a67223a5c3884ae59f1b5d781da79
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-10-23 14:28:08 +0200

    martedì pranzo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,102 @@
+var number;
+var s=25;
+
 function preload(){
-  // put preload code here
+  sound = loadSound('assets/Piano Music - Paradise - Sad Instrument Piano.mp3');
 }
 
-function setup() {
-  // put setup code here
+function setup(){
+  var cnv = createCanvas(windowWidth,windowHeight);
+  cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(0.2);
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+  background(0);
+  var w=windowWidth/50;
+
+  var spectrum = fft.analyze();
+
+  stroke(0);
+  for (var i = 0; i< spectrum.length; i++){
+    var amp = spectrum[i];
+    var y = map(amp,0,256,windowHeight,0);
+  //line(i*w,windowHeight,i*w,y);
+  if (y>windowHeight/2) {
+    fill(255);
+  }
+  else {
+    fill(0,255,0);
+  }
+  //rect(i*w,y,w,windowHeight-y);
+  rect(i*w,windowHeight/2,w,windowHeight);
+  // tasti neri
+  if (y>windowHeight/2) {
+    fill(0);
+  }
+  else {
+    fill(50);
+  }
+  //tasti neri 
+  rect(s,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+3*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+4*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+5*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+7*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+8*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+10*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+11*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+12*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+14*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+15*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+17*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+18*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+19*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+21*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+22*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+24*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+25*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+26*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+28*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+29*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+31*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+32*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+33*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+35*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+36*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+38*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+39*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+40*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+42*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+43*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+45*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+46*w,windowHeight/2,w*4/6,windowHeight/4);
+  rect(s+47*w,windowHeight/2,w*4/6,windowHeight/4);
+
+  }
+
+  /*var waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255,0,0); // waveform is red
+  strokeWeight(1);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map( waveform[i], -1, 1, 0, height);
+    vertex(x,y);
+  }
+  endShape();*/
+
+  text('click to play/pause', 4, 10);
+}
+
+// fade sound if mouse is over canvas
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
 }
